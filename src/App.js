c98aa9a156b9b84ef81f95ec1867ffac34fd88d5
ando-woods <73496577+Ando-O@users.noreply.github.com>
2021-11-08 21:48:07 +0000

    created createKeyPairs.js created keypair.json changed and deleted: let baseAccount = Keypair.generate() to have new consts that can persist keypair

--- src/App.js
+++ src/App.js
@@ -2,20 +2,25 @@ import { useEffect, useState } from 'react';
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 import { async } from 'q';
-
 import { Connection, PublicKey, clusterApiUrl} from '@solana/web3.js';
 import {
   Program, Provider, web3
 } from '@project-serum/anchor';
 
 import idl from './idl.json';
+import kp from './keypair.json'
 
 // Constants
 // SystemProgram is a reference to the Solana runtime.
 const { SystemProgram, Keypair } = web3;
 
 // Create a keypair for the account that will hold the GIF data.
-let baseAccount = Keypair.generate();
+//let baseAccount = Keypair.generate();
+
+// Persist keypair.
+const arr = Object.values(kp._keypair.secretKey)
+const secret = new Uint8Array(arr)
+const baseAccount = web3.Keypair.fromSecretKey(secret)
 
 // Get our program's id form the IDL file.
 const programID = new PublicKey(idl.metadata.address);
